6fe6e7d71f0ed3e0aa9936b1bd7069dda9332fed
Souradip <dgsouro77@gmail.com>
2024-10-12 21:36:55 +0530

    mentioned the routes for the app

---
+++ week-7-mongodb-7.1/index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+
+const app=express();
+
+app.use(express.json());
+
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signin", (req, res) => {
+    
+
+})
+
+app.post("/todos", (req, res) => {
+
+})
+
+
+app.get("/todos", (req, res) => {
+
+})
+
+
+    
