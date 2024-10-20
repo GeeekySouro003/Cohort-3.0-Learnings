78a83451c347e8b4abc2d241196181af41e3e1c8
Souradip <dgsouro77@gmail.com>
2024-10-21 02:05:27 +0530

    Added the course add endpoint

--- week-8-course-selling-app/routes/admin.js
+++ week-8-course-selling-app/routes/admin.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
 const adminRouter=Router();
-const {adminModel}= require('../db.js');
+const {adminModel, courseModel}= require('../db.js');
 const jwt=require('jsonwebtoken');
 const {JWT_ADMIN_SECRET}=require('../config.js');
+const { adminMiddleware } = require('../middleware/admin.js');
+const course = require('./course.js');
 adminRouter.post("/signup", async function(req, res) {
     const{email,password,firstName,lastName}=req.body;
     await adminModel.create({email: email, password: password, firstName: firstName,lastName: lastName});
@@ -37,9 +39,20 @@ adminRouter.post("/signin", async function(req, res) {
         }
 })
 
-adminRouter.post("/course", function(req, res) {
+adminRouter.post("/course",adminMiddleware, async function(req, res) {
+    const adminId=req.userId;
+
+    const {title,description,price,imageUrl}=req.body;
+    await courseModel.create({
+      title: title,
+      description: description,
+      price: price,
+      imageUrl: imageUrl,
+      creatorId:adminId
+    })
     res.json({
-        message:"Sign up endpoint"
+        message:"Course created",
+        courseId:course._id
     })
 })
 
