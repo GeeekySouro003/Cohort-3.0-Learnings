a9c67ae530f065a6847379c5c7f7be53fc23c6e5
Souradip <dgsouro77@gmail.com>
2024-11-02 22:17:20 +0530

    Made a simple otp counter .Improvements to be done

--- week-13-tailwind/tailwind_learning/src/App.jsx
+++ week-13-tailwind/tailwind_learning/src/App.jsx
@@ -1,26 +1,14 @@
+
 import './App.css'
+import { Otp } from './components/Otp'
 
 function App() {
 
   return (
-    <>
-     {/* <div className='bg-blue-500'>Hi</div>  */}
-      <div className=' sm:grid-cols-12 grid grid-cols-1'>
-      <div className='col-span-1 bg-blue-700 sm:col-span-5'>
-        child 1
-      </div>
-      <div className='col-span-1 bg-red-500 sm:col-span-5'>
-        child 2
-      </div>
-      <div className='col-span-1 bg-green-400 sm:col-span-2'>
-        child 3
-      </div>
-     </div>
-
-     {/* <div className='md:bg-green-400 sm:bg-blue-400 bg-red-700'>
-      hello there
-     </div> */}
-    </>
+    <div className='h-screen bg-blue-700'>
+      <br/><br/><br/><br/>
+      <Otp number={20}/>
+    </div>
   )
 }
 
