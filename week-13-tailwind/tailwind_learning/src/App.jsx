40493ff003a8e081b8a60edff6f1c28abb831e76
Souradip <dgsouro77@gmail.com>
2024-11-02 20:43:06 +0530

    Basic in tailwind

--- week-13-tailwind/tailwind_learning/src/App.jsx
+++ week-13-tailwind/tailwind_learning/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     {/* <div className='bg-blue-500'>Hi</div> */}
+     <div className='grid grid-cols-12'>
+      <div className='bg-blue-700 col-span-4'>
+        child 1
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='bg-red-500 col-span-5'>
+        child 2
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='bg-green-400 col-span-3'>
+        child 3
+      </div>
+     </div>
     </>
   )
 }
