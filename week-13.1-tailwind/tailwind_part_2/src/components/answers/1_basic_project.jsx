ac15e608ec2fc9d51683f8374dcead8c1f4aca1d
Souradip <dgsouro77@gmail.com>
2024-11-03 21:16:27 +0530

    Sidebar transition and animation

---
+++ week-13.1-tailwind/tailwind_part_2/src/components/answers/1_basic_project.jsx
@@ -0,0 +1,12 @@
+export function SidebarClass1() {
+    return <div className="flex">
+
+            <div className="transition-all delay-1000 md:-translate-x-0 md:w-96 -translate-x-96 h-screen w-0 ">
+                Sidebar
+            </div>
+            <div className="bg-blue-400 w-full h-screen">
+                Content
+            </div>
+        </div>
+    
+}
--- week-13.1-tailwind/tailwind_part_2/tailwind.config.js
+++ week-13.1-tailwind/tailwind_part_2/tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          200: "#8094ad",
+          500: "#19406a",
+          700: "#002b5b",
+        },
+        green: {
+          400: "#36c6c0"
+        },
+        slate: {
+          200: "#e4e8ee",
+        }
+
+      }
+    },
   },
   plugins: [],
 }
-
