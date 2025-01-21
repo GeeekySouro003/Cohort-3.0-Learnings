1c6844dd3e55b20c12f3507df4cc7965b4d9cc33
Souradip <dgsouro77@gmail.com>
2025-01-21 22:13:50 +0530

    Using context api

--- week-10.1-react-contextapi/react-part-4/src/App.jsx
+++ week-10.1-react-contextapi/react-part-4/src/App.jsx
@@ -1,51 +1,133 @@
-import { createContext, useContext, useState } from 'react';
-import './App.css';
+import React, { createContext, useContext, useState } from 'react';
 
-const BulbContext=createContext();
-function App() {
-  const[bulbOn,setbulbOn]=useState(false); //returns an array[] 
-return (
-  <div>
-    <BulbContext.Provider value={{
-      bulbOn:bulbOn,
-      setbulbOn:setbulbOn
-    }}>
-    <LightBulb/>
-    </BulbContext.Provider>
-  
-  </div>
-  
-)
+const CountContext = createContext();
+
+function CountContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+
+  return <CountContext.Provider value={{count, setCount}}>
+    {children}
+  </CountContext.Provider>
 }
-//Rolling up the state to the lowest common  ancestor of the two children possivble
-function LightBulb() {
 
+function Parent() {
+  return (
+    <CountContextProvider>
+      <Increase />
+      <Decrease />
+      <Value />
+    </CountContextProvider>
+  );
+}
+
+function Decrease() {
+  const {setCount} = useContext(CountContext);
+  return <button onClick={() => setCount(count=>count - 1)}>Decrease</button>;
+}
+
+function Increase() {
+  const {setCount} = useContext(CountContext);
+  return <button onClick={() => setCount(count=>count + 1)}>Increase</button>;
+}
+
+function Value() {
+  const {count} = useContext(CountContext);
+  return <p>Count: {count}</p>;
+}
+
+// App Component
+const App = () => {
   return <div>
-    <BulbState/>
-    <ToggleBulbState/>
+    <Parent />
   </div>
-}
-
-function BulbState({bulbOn}) {
-  const {bulbOn}=useContext(BulbContext)
-return <div>
-{
-  bulbOn?"Bulb on":"Bulb off"
-}
-</div>
-}
-
-function ToggleBulbState({bulbOn,setbulbOn}) {
-
-  const {bulbOn,setbulbOn}=useContext(BulbContext);
-  function toggle() {
-    setbulbOn(!bulbOn);
-  }
-  return<div>
-    <button onClick={toggle}>
-      Toogle the button
-    </button>
-  </div>
-}
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createContext, useContext, useState } from 'react';
+// import './App.css';
+
+// const BulbContext=createContext();
+
+
+// //creating custom context providers
+
+// function BulbProvider({children}) { // wrapper component
+//   const[bulbOn,setbulbOn]=useState(false);
+//   return <BulbContext.Provider value={{
+//     bulbOn:bulbOn,
+//     setbulbOn:setbulbOn
+//   }}>
+//  {children}
+//   </BulbContext.Provider>
+
+// }
+// function App() {
+//    //returns an array[] 
+// return (
+//   <div>
+//     <BulbProvider>
+//       <LightBulb/>
+//     </BulbProvider>
+    
+//   </div>
+  
+// )
+// }
+// //Rolling up the state to the lowest common  ancestor of the two children possivble
+// function LightBulb() {
+
+//   return <div>
+//     <BulbState/>
+//     <ToggleBulbState/>
+//   </div>
+// }
+
+// function BulbState() {
+//   const {bulbOn}=useContext(BulbContext)
+// return <div>
+// {
+//   bulbOn?"Bulb on":"Bulb off"
+// }
+// </div>
+// }
+
+// function ToggleBulbState() {
+
+//   const {bulbOn,setbulbOn}=useContext(BulbContext);
+//   function toggle() {
+//     setbulbOn(!bulbOn);
+//   }
+//   return<div>
+//     <button onClick={toggle}>
+//       Toogle the button
+//     </button>
+//   </div>
+// }
+
+// export default App;
