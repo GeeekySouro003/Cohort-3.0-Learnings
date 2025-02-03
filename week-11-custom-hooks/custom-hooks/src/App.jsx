e32d15465ad561ffc90fd38db1a2ab722eebc55b
Souradip <dgsouro77@gmail.com>
2025-02-03 21:38:35 +0530

    Implementation of useprev by harkirat

--- week-11-custom-hooks/custom-hooks/src/App.jsx
+++ week-11-custom-hooks/custom-hooks/src/App.jsx
@@ -3,23 +3,25 @@ import './App.css'
 import {  useFetch, } from './hooks/useFetch'
 import { usePrev } from './hooks/use-prev'
 
-function App() {
-  const[state,setState]=useState(0);
-  const prev=usePrev(state);
 
-  return (
-    <>
-     <p>{state}</p>
-     <button onClick={() => {
-      setState(currentState=>currentState+1);
-     }} >Click Me</button>
-     <p>The previous value was {prev}</p>
-    </>
+//UsPreviou Hooks implementation
+// function App() {
+//   const[state,setState]=useState(0);
+//   const prev=usePrev(state);
+
+//   return (
+//     <>
+//      <p>{state}</p>
+//      <button onClick={() => {
+//       setState(currentState=>currentState+1);
+//      }} >Click Me</button>
+//      <p>The previous value was {prev}</p>
+//     </>
    
-  )
-}
+//   )
+// }
 
-export default App;
+// export default App;
 
 
 
--- week-11-custom-hooks/custom-hooks/src/hooks/use-prev.js
+++ week-11-custom-hooks/custom-hooks/src/hooks/use-prev.js
@@ -1,8 +1,10 @@
 import { useEffect, useRef } from "react"
 
 export const usePrev = (value) => {
-    const ref=useRef();
+    const ref=useRef(); //ref to store a value when u update the value woh kabhi re-render nahi hoga
 
+    
+// it returns first and the effect will called later
     useEffect(()=> {
         ref.current=value;
     }, [value]);
