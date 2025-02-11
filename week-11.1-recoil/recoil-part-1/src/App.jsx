eb14a9d21ca981ba21fffe68b3dd7f77c7b7d007
Souradip <dgsouro77@gmail.com>
2025-02-11 22:36:04 +0530

    bug in currentCount component

--- week-11.1-recoil/recoil-part-1/src/App.jsx
+++ week-11.1-recoil/recoil-part-1/src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
 import './App.css'
-
+import {RecoilRoot, useRecoilValue, useSetRecoilState} from 'recoil'
+import { counterAtom } from './store/atoms/counter'
 function App() {
 
   return (
-    <>
+    <RecoilRoot>
     <Counter/>
-    </>
+    </RecoilRoot>
   )
 }
 
 
 function Counter() {
-  const[count,setCount]=useState(0);
+  //const[count,setCount]=useState(0);
+  //Creating a atom for counter
   return <div>
-    <CurrentCount count={count} />
+    <CurrentCount  />
     <br/>
-    <Increase setCount={setCount}/>
+    <Increase />
     <br/>
-    <Decrease setCount={setCount}/>
+    <Decrease/>
   </div>
 }
 
-function CurrentCount({count}){
+function CurrentCount(){
+  const count=useRecoilValue(counterAtom);
+  console.log("count is working")
   return<div>
     {count}
   </div>
 }
-function Increase({setCount}) {
+function Increase() {  //Subscribed to the setter
+ 
+  const setCount=useSetRecoilState(counterAtom);
   function increase() {
     setCount(c=>c+1);
   }
@@ -36,7 +42,8 @@ function Increase({setCount}) {
   </div>
 }
 
-function Decrease({setCount}) {
+function Decrease() {
+  const setCount=useSetRecoilState(counterAtom);
 
   function decrease() {
     setCount(c=>c-1);
