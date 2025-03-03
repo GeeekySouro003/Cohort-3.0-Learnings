f08f6aa36182da271a9229f915d1b800e882e996
Souradip <dgsouro77@gmail.com>
2025-03-03 13:01:24 +0530

    Implemented the counter using usestate

---
+++ week-11.1-recoil/recoil-part-1/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+
+function Counter() {
+  const[count,setCount]=useState(0);
+  return <div>
+    <CurrentCount count={count} />
+    <Increase setCount={setCount}/>
+    <Decrease setCount={setCount}/>
+  </div>
+}
+function CurrentCount({count}) {
+return <div>
+  {count}
+</div>
+}
+
+function Increase({setCount}) {
+
+  function increase() {
+    setCount(c=>c+1);
+  }
+  return <div>
+    <button onClick={increase}>Increase</button>
+  </div>
+}
+
+function Decrease({setCount}) {
+  function decrease() {
+    setCount(c=>c-1);
+  }
+  return <div>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+export default App
