4c744c2fccce6853b9f058b2d49e8deb02aa9c74
Souradip <dgsouro77@gmail.com>
2025-03-11 22:19:54 +0530

    Solved the first greet function of ts

--- week-14-typescript/index.js
+++ week-14-typescript/index.js
@@ -1,12 +1,8 @@
+"use strict";
 // let x:number | string  =1; //defining types in typescript(type inferencing)
-
 // x="souradip";
 // console.log(x);
-
-
-//Write a function that greets a user given their first name. 
-function Greet(firstName:string) {
+function Greet(firstName) {
     console.log("hello" + firstName);
-
 }
 Greet('Souradip');
