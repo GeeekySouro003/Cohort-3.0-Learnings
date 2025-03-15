ec786cb75bbdd6fe6497d4544eb531b3a5a91c9a
Souradip <dgsouro77@gmail.com>
2025-03-15 22:58:47 +0530

    changed target to 2020 in tsconfig

--- week-14-typescript/index.js
+++ week-14-typescript/index.js
@@ -1,30 +1,35 @@
 "use strict";
-// let x:number | string  =1; //defining types in typescript(type inferencing)
-// x="souradip";
-// console.log(x);
-//Write a function that greets a user given their first name. 
-function Greet(firstName) {
-    console.log("hello" + firstName);
-}
-Greet('Souradip');
-//Write a function that calculates the sum of two functions
-function TwoSum(a, b) {
-    return a + b;
-}
-console.log(TwoSum(5, 7));
-//Return true or false based on if a user is 18+
-function isLegal(age) {
-    if (age > 18) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-console.log(isLegal(21));
-function Another(fn) {
-    setTimeout(fn, 1000);
-}
-Another(function () {
-    console.log("heyyyy bhiduu");
-});
+// // let x:number | string  =1; //defining types in typescript(type inferencing)
+// // x="souradip";
+// // console.log(x);
+// //Write a function that greets a user given their first name. 
+// function Greet(firstName:string) {
+//     console.log("hello" + firstName);
+// }
+// Greet('Souradip');
+// //Write a function that calculates the sum of two functions
+// function TwoSum(a:number,b:number):number {
+// return a+b;
+// }
+// console.log(TwoSum(5,7));
+// //Return true or false based on if a user is 18+
+// function isLegal(age:number):boolean { //boolean is the return type of the function
+//     if(age>18) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// console.log(isLegal(21));
+// //Create a function that takes another function as input, and runs it after 1 second.
+// function Another(fn:()=>void ) {
+//     setTimeout(fn,1000);
+// }
+// Another(function() {
+//     console.log("heyyyy bhiduu")
+// })
+//tsconfig file 
+let greet = () => {
+    console.log('Hi there!');
+};
