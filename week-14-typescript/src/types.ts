daf010823c77ef3ca51a58dbf86ffbd9465b9dfd
Souradip <dgsouro77@gmail.com>
2025-04-16 21:44:50 +0530

    Done with types and its specialties

---
+++ week-14-typescript/src/types.ts
@@ -0,0 +1,30 @@
+//types let u do union which is either of the two can be a possible action
+
+type StringOrNumber = string | number;
+
+function printId(id: StringOrNumber) {
+  console.log(`ID: ${id}`);
+}
+
+printId(101); // ID: 101
+printId("202"); // ID: 202
+
+//intersection which is both and a common betwenn them
+type Employee = {
+    name: string;
+    startDate: Date;
+  };
+  
+  type Manager = {
+    name: string;
+    department: string;
+  };
+  
+  type TeamLead = Employee & Manager;
+  
+  const teamLead: TeamLead = {
+    name: "harkirat",
+    startDate: new Date(),
+    department: "Software developer"
+  };
+  
